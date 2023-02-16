b6e1fe872c1859c9cc48565f5cea709f1c909aa2
yuchen <yuchenz27@outlook.com>
2023-02-16 11:49:16 +0800

    Initial Commit

---
+++ WitSensorUnityPlugin/WitSensorUnityPlugin.h
@@ -0,0 +1,12 @@
+//
+//  WitSensorUnityPlugin.h
+//  WitSensorUnityPlugin
+//
+//  Created by Yuchen Zhang on 2023/2/16.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WitSensorUnityPlugin : NSObject
+
+@end
---
+++ WitSensorUnityPlugin/WitSensorUnityPlugin.m
@@ -0,0 +1,12 @@
+//
+//  WitSensorUnityPlugin.m
+//  WitSensorUnityPlugin
+//
+//  Created by Yuchen Zhang on 2023/2/16.
+//
+
+#import "WitSensorUnityPlugin.h"
+
+@implementation WitSensorUnityPlugin
+
+@end
